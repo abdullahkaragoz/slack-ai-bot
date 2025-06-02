988390a7113a9093f14da2737572662d8e0399f0
Abdullah Karagoz <abdullah.karagoz@outlook.com>
2025-06-02 16:16:59 +0300

    Update slack_bot.py

--- slack_bot.py
+++ slack_bot.py
@@ -2,9 +2,9 @@ import requests
 import os
 
 # Ortam değişkenleri
-SLACK_BOT_TOKEN = os.environ["xoxp-8967571839827-8967571843395-8983530150789-60ee0dad19af372fa356ebe13db910d3"]
-SLACK_CHANNEL_ID = os.environ["C08UFGTUXV1"]
-OPENROUTER_API_KEY = os.environ["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"]
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_CHANNEL_ID = os.environ["SLACK_CHANNEL_ID"]
+OPENROUTER_API_KEY = os.environ["OPENROUTER_API_KEY"]
 
 # Yapay zeka mesajı üret
 def generate_message():
